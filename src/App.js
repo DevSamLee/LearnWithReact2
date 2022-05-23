8bbf2bccea48707f498495357fbc604c30576dd8
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-05-22 23:50:52 -0500

    20220522
    Add useEffect to search the keyword.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("render");
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("I run all the time.");
+  useEffect(() => {
+    console.log("CALL THE API.");
+  }, []);
+  useEffect(() => {
+  console.log("Search for", keyword);
+  }, [keyword]);
+  
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here" />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
