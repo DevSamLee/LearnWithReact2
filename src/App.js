6913c0920e1cd44092b1a61a5ffb25a2b16b5d27
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-05-23 23:25:02 -0500

    20220523
    Add hide and show button with clean up function.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import {useState, useEffect} from "react";
 
+function MyName() {
+  useEffect(() => {
+    console.log("Created");
+    // clean up function
+    return () => console.log("destroyed");
+  }, []);
+  return <h1>Hello, my name is Sam.</h1>;
+}
+
 function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
   const onClick = () => setValue((prev) => prev + 1);
+  const onClick2 = () => setShowing((prev) => !prev);
   const onChange = (event) => setKeyword(event.target.value);
   console.log("I run all the time.");
   useEffect(() => {
@@ -18,6 +29,8 @@ function App() {
       <input value={keyword} onChange={onChange} type="text" placeholder="Search here" />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
+      {showing ? <MyName /> : null}
+      <button onClick={onClick2}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
