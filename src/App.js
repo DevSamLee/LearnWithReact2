c3f2db92617377e777825ac184424f366306a366
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-05-28 23:53:29 -0500

    20220528
    Add list of coins from json file.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
     .then((response) => response.json())
@@ -9,12 +10,16 @@ function App() {
       setCoins(json);
       setLoading(false);
     });
-  }, []);
+  }, []); 
   return (
   <div>
     <h1>The Coins!</h1>
-    {loading ? <strong>Loading...</strong> : null}
-  </div>
+    {loading ? <strong>Loading...</strong> : (
+    <ul>
+      {coins.map((coin) => <li>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</li>)}
+    </ul>
+    )}
+  </div>  
   );
 }
 
